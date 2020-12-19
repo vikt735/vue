3036371f7d631bf9373879fb6b2e55d646599bec
vikt735 <65780908+vikt735@users.noreply.github.com>
2020-12-19 19:59:58 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:/vikt735/projectVue.git master:gh-pages
+git push -f git@github.com:/vikt735/vue.git master:gh-pages
 
 cd -
